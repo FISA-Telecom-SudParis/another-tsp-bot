57bb1e75fae8cb5c816b634643408eb4b09bc830
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2024-11-12 14:37:00 +0100

    fix: avoid webhook to recreate itself (on up)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,4 @@
 # git pull origin master
 ./bin/compose-prod.sh --profile webhook pull
 ./bin/compose-prod.sh down
-./bin/compose-prod.sh --profile webhook up -d
+./bin/compose-prod.sh --profile webhook up --no-recreate --no-build --remove-orphans -d
